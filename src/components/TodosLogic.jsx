1426077204f601b05a5746c9087df9c5ea2d6abd
Joy Phoebe <joyapisi@gmail.com>
2023-05-01 13:01:58 +0300

    add to do app components

---
+++ src/components/TodosLogic.jsx
@@ -0,0 +1,33 @@
+import InputTodo from './InputTodo';
+import TodosList from './TodosList';
+const TodosLogic = () => {
+    const todos = [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ];
+    return (
+        <div>
+      <InputTodo />
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
+      <TodosList todosProps={todos} />
+      </div>
+    )
+  }
+  export default TodosLogic;
